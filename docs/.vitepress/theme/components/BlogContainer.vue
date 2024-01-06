7c5d8e57de6b5776b534ec942c587ab07f6ac976
jinesh subedi <jinesh.subedi@metacloud.com.np>
2024-01-06 18:40:04 +0545

    update blog

--- docs/.vitepress/theme/components/BlogContainer.vue
+++ docs/.vitepress/theme/components/BlogContainer.vue
@@ -3,24 +3,15 @@
             <div class="mx-auto max-w-7xl px-6 lg:px-8">
                   <div
                         class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-                        <ArticleCard title="Laravel Google 2FA Authentication Tutorial"
-                              excerpt="Laravel is a secure framework with security features. However, if you want to make your laravel app more secure..."
-                              href="./laravel-google-2fa-auth" 
-                              date="2024-01-06" 
-                              tag="Laravel, Google, Two Factor Auth, Google Auth" 
+                              <ArticleCard 
+                                    v-for="(blog, index) in articles" :key="index"
+                                    :title="blog.title"
+                                    :excerpt="blog.excerpt"
+                                    :href="blog.href"
+                                    :date="blog.date"
+                                    :tag="blog.tag"
                         />
-                        <ArticleCard title="Changing PHP Version in Ubuntu - A Step-by-Step Guide"
-                              excerpt="If you're running a web server on Ubuntu and need to upgrade your PHP version, this guide will walk you through..."
-                              href="./change-php-version-in-ubuntu" 
-                              date="2024-01-03" 
-                              tag="PHP, Ubuntu, Server" 
-                        />
-                        <ArticleCard title="Oracle to PostgreSQL SQL Converter Tool"
-                              excerpt="If you're running a web server on Ubuntu and need to upgrade your PHP version, this guide will walk you through..."
-                              href="./oracletopgsql" 
-                              date="2024-01-01" 
-                              tag="Oracle, PostgreSQL, SQL, Conversion" 
-                        />
+                        
                   </div>
             </div>
             <div>
@@ -30,4 +21,29 @@
             </div>
       </div>
 </template>
+<script setup>
+const articles = [
+      {
+            title: "Laravel Google 2FA Authentication Tutorial",
+            excerpt: "Laravel is a secure framework with security features. However, if you want to make your laravel app more secure...",
+            href: "./laravel-google-2fa-auth",
+            date: "2024-01-06",
+            tag: "Laravel, Google, Two Factor Auth, Google Auth"
+      },
+      {
+            title: "Changing PHP Version in Ubuntu - A Step-by-Step Guide",
+            excerpt: "If you're running a web server on Ubuntu and need to upgrade your PHP version, this guide will walk you through...",
+            href: "./change-php-version-in-ubuntu",
+            date: "2024-01-03",
+            tag: "PHP, Ubuntu, Server"
+      },
+      {
+            title: "Oracle to PostgreSQL SQL Converter Tool",
+            excerpt: "If you're transitioning from Oracle to PostgreSQL and need to convert your SQL queries, this simple tool can...",
+            href: "./oracletopgsql",
+            date: "2024-01-01",
+            tag: "Oracle, PostgreSQL, SQL, Conversion"
+      },
+];
+</script>
 
